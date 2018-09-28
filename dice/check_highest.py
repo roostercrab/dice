1a8aaff1bd5db0fcfd61eb9fefea80e80073ed05
Jesse <jesse.shea@qixingdesign.com>
2018-09-28 12:24:05 -0700

    moved dictionary around

--- dice/check_highest.py
+++ dice/check_highest.py
@@ -1,24 +1,26 @@
+highest_results_dictionary = {  
+'d4s_highest':0,
+'d6s_highest':0,
+'d8s_highest':0,
+'d10s_highest':0,
+'d12s_highest':0,
+'d20s_highest':0,
+'d4_hits':0,
+'d6_hits':0,
+'d8_hits':0,
+'d10_hits':0,   
+'d12_hits':0,
+'d20_hits':0,  
+'quint_hit':0, 
+'quad_hit':0,
+'triple_hit':0,
+'double_hit':0,
+'max_highest':0}
+
+
 def CheckHighest(d4s, d6s, d8s, d10s, d12s, d20s):
     #print('made it to check highest')
     #checking for the highest roll of the bunch
-    highest_results_dictionary = {  
-    'd4s_highest':0,
-    'd6s_highest':0,
-    'd8s_highest':0,
-    'd10s_highest':0,
-    'd12s_highest':0,
-    'd20s_highest':0,
-    'd4_hits':0,
-    'd6_hits':0,
-    'd8_hits':0,
-    'd10_hits':0,   
-    'd12_hits':0,
-    'd20_hits':0,  
-    'quint_hit':0, 
-    'quad_hit':0,
-    'triple_hit':0,
-    'double_hit':0,
-    'max_highest':0}
     
     max_highest = 0
     
@@ -109,8 +111,6 @@ def CheckHighest(d4s, d6s, d8s, d10s, d12s, d20s):
     double_hit,
     max_highest]    
 
-    return(highest_results_list)
-
     list_counter = 0    
     for k, v in highest_results_dictionary.items():
         #print('this is the key: %s' % k)
@@ -136,17 +136,11 @@ def CheckHighest(d4s, d6s, d8s, d10s, d12s, d20s):
 
     return(highest_return_dictionary)
 
-
-
-
 def ClearHighest():
     highest_results_dictionary.clear()
     #print('clearing highest: %s' % highest_results_dictionary)
     
     return()
-    
-     
-'''
     
     
     
