6870241777ec834694dbb5f7e220090e1dc656fc
Jesse <jesse.shea@qixingdesign.com>
2018-09-28 12:17:34 -0700

    removed triple quotes from stage

--- dice/stage_function.py
+++ dice/stage_function.py
@@ -1,18 +1,8 @@
 from master_roll import Roll
 from check_highest import CheckHighest
 
-
-'''
-from check_highest import ClearHighest
-'''
-
 def Stage(staged_dicelist, number_of_rolls):
-    
-    '''
-    #resets the dice metrics for the next roll
-    ClearHighest()
-    '''
-    
+
     #staged_dicelist contains Dice objects and will iterate through the list rolling each one in a group then passing them to the check functions to be analyzed    
     #iterates over the actual rolls, number_of_rolls is passed from the beginning in Start_Rolls(number_of_rolls)
     for roll_number in range(number_of_rolls):
